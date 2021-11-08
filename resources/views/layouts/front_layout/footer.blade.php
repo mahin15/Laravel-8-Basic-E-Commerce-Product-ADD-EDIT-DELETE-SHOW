258e159596278ab1be0aa25e89ae239c58bc1332
Saieedul Hasan Mahin <intern61.it@akij.net>
2021-11-08 12:36:23 +0600

    updated

--- resources/views/layouts/front_layout/footer.blade.php
+++ resources/views/layouts/front_layout/footer.blade.php
@@ -6,7 +6,7 @@
                 <div class="row">
                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
 
-                        <img class="logo1" src="{{asset('images/front_images/img/logo1.jpg')}}" />
+                        <img class="logo1" src="{{asset('images/front_images/img/ecom3.png')}}" />
                     </div>
 
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
@@ -22,10 +22,8 @@
 
                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
                         <ul class="sociel">
-                            <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
-                            <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
-                            <li> <a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li> <a href="https://www.instagram.com/saieedul_mahin/"><i class="fa fa-instagram"></i></a></li>
+                            <li> <a href="https://www.linkedin.com/in/saieedulmahin/"><i class="fa fa-linkedin"></i></a></li>
                         </ul>
                     </div>
                 </div>
@@ -54,7 +52,7 @@
         </div>
       
         <div class="copyright">
-            <p>Copyright @ 2021 All Right Reserved By Saieedul Hasan Mahin</p>
+            <p><a href="https://www.linkedin.com/in/saieedulmahin/">Copyright @ 2021 All Right Reserved By Saieedul Hasan Mahin</a> </p>
         </div>
 
  </div>
